2ff4de9b693f8a18eb93ee4fa14c043e08ea1b66
eriollee <eriollee@163.com>
2018-01-17 16:34:29 +0800

    图像识别

---
+++ src/pages/Home.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="home">
+    <Row class="header" type="flex" align="middle">
+      <div class="logo">
+        <img src="../assets/img/logo.png" height="30" alt="">
+        <span>VUE</span>MANAGER <Tag>beta</Tag>
+      </div>
+      <VmMsgPush style="margin-left:20px" :data="msgPushData"></VmMsgPush>
+      <Dropdown class="login-info" placement="bottom-end">
+        <Button type="ghost">
+            <img src="../assets/img/photo.jpg" height="30" alt="">Admin
+            <Icon type="arrow-down-b"></Icon>
+        </Button> 
+        <Dropdown-menu slot="list">
+            <Dropdown-item divided><i class="fa fa-key"></i>Log out</Dropdown-item>
+            <Dropdown-item divided><i class="fa fa-cog"></i>Setting</Dropdown-item>
+        </Dropdown-menu>
+      </Dropdown>
+    </Row>   
+    <div class="sidebar">
+      <Menu theme="dark" width="100%" class="menu" :active-name="activeName" :accordion="true">
+          <Menu-item name="ImageList">
+              <router-link to="/imagelist">
+                <i class="fa fa-photo"></i>
+                Image List  
+              </router-link>
+          </Menu-item>
+      </Menu>
+    </div>
+    <div class="main-content">
+      <router-view></router-view> 
+    </div>
+  </div>
+  </div>
+</template>
+<script>
+import VmMsgPush from '@/components/vm-msg-push.vue'
+export default {
+  name: 'home',
+  components: {
+    VmMsgPush
+  },
+  mounted: function () {
+    this.activeName = this.$route.name
+  },
+  data () {
+    return {
+      activeName: 'Dashboard',
+      msgPushData: [
+        {
+          image: require('@/assets/img/photo.jpg'),
+          from: 'JesseLuo',
+          time: '2017-1-8',
+          message: 'I like your website very much!'
+        },
+        {
+          image: require('@/assets/img/photo.jpg'),
+          from: 'JesseLuo',
+          time: '2017-1-8',
+          message: 'I like your website very much!'
+        },
+        {
+          image: require('@/assets/img/photo.jpg'),
+          from: 'JesseLuo',
+          time: '2017-1-8',
+          message: 'I like your website very much!'
+        },
+        {
+          image: require('@/assets/img/photo.jpg'),
+          from: 'JesseLuo',
+          time: '2017-1-8',
+          message: 'I like your website very much!'
+        },
+        {
+          image: require('@/assets/img/photo.jpg'),
+          from: 'JesseLuo',
+          time: '2017-1-8',
+          message: 'I like your website very much!'
+        }
+      ]
+    }
+  }
+}
+</script>
